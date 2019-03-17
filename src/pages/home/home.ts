b818340381a53b0ca66f0a44901c2768acfe898b
thong2539 <phathong5562@gmail.com>
2019-03-17 14:25:27 +0700

    Generate Page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AboutPage } from '../about/about';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,11 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  gotoAbout(){
+    this.navCtrl.push(AboutPage);
+    
   }
 
 }
